7fcdcbbe58dd04dd350fe23f50d3085a58910ef0
dragon <1183665002@qq.com>
2022-07-15 22:45:15 +0800

    商品图片渲染

--- components/ContentScreen/HomeScreen/ProductDetailsScreen.js
+++ components/ContentScreen/HomeScreen/ProductDetailsScreen.js
@@ -1,37 +1,42 @@
 import * as React from 'react';
-import { Button, View, Text, Image, Dimensions } from 'react-native';
+import { SafeAreaView, ScrollView, View, Text, Image, Dimensions } from 'react-native';
 import { GetProductById } from '../../../api/index'
 
 const w = Dimensions.get("window")
 
 export default function ProductDetailsScreen({ navigation }) {
     const [product, setProduct] = React.useState({})
+    const [akey, setAkey] = React.useState([])
     React.useEffect(() => {
         GetProductById("/product/queryProductById", { params: { productId: 1 } })
             .then((res) => {
                 setProduct(res)
+                setAkey(Object.keys(res))
             })
         return
     }, [])
-    let key = Object.keys(product)
-    if (key.length) {
-
-        console.log("product.productRotationImg[0]", product?.productRotationImg[0])
+    React.useEffect(() => {
+        asd()
+    }, ['akey'])
+    const ResView = (props) => {
+        const { myKey } = props
+        if (myKey.indexOf("Img") != -1) return <Image style={{ width: w.width, height: w.width }} source={{ uri: product[myKey][0] }} />
+        else return <Text>{product[myKey]}</Text>
     }
+    const asd = () => {
+        let arr = akey.map((val, index) => <ResView key={index} myKey={val} />)
+        console.log(...arr)
+    }
+
     return (
-        <View>
-            <Text>productDetailsScreen</Text>
-            <View>
-                {/* {key.length ? key.map((val, index) => <Text key={index}>{product[val]}</Text>) : <Text>123</Text>} */}
-            </View>
-            <View>
-                <Image
-                    style={{ width: w.width, height: w.width }}
-                    source={{
-                        uri: "http://150.158.96.29:8082/my-shop-img/uploadProductRotationImg/2f0aecaef4514b46b25fb86484e60a1d.jpg",
-                    }}
-                />
-            </View>
-        </View>
+        <SafeAreaView >
+            <ScrollView  >
+                <Text>productDetailsScreen</Text>
+                <View>
+                    {akey.length ?  akey.map((val, index) => <ResView key={index} myKey={val} />) : <Text ></Text>}
+                </View>
+            </ScrollView>
+        </SafeAreaView>
     );
 }
+
